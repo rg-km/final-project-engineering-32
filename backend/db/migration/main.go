e407522c9b64a96fec74d6282ea97aef9bdaed55
Muhammad Wahyu Agustino <wahyuagustino33@gmail.com>
2022-06-17 21:19:57 +0700

    repository

--- backend/db/migration/main.go
+++ backend/db/migration/main.go
@@ -14,41 +14,42 @@ func main() {
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS users (
-    id integer not null primary key AUTOINCREMENT,
-    username varchar(255) not null,
-    password varchar(255) not null,
-    role varchar(255) not null,
-    loggedin boolean not null
+	CREATE TABLE IF NOT EXISTS user (
+    id_user integer not null primary key AUTOINCREMENT,
+    username text(255) not null,
+    password text(255) not null,
+    role varchar(255) not null
 );
 
-CREATE TABLE IF NOT EXISTS products (
-    id integer not null primary key AUTOINCREMENT,
-    product_name varchar(255) not null,
-    category varchar(255) not null,
-    price integer not null,
-    quantity integer not null
+CREATE TABLE IF NOT EXISTS buku (
+    id_buku integer not null primary key AUTOINCREMENT,
+    pengarang text(255) not null,
+    judul_buku text(255) not null,
+    tahun_terbit integer not null,
+    no_buku text not null
 );
 
-CREATE TABLE IF NOT EXISTS cart_items (
-    id integer not null primary key AUTOINCREMENT,
-    product_id integer not null,
-    quantity integer not null,
-    FOREIGN KEY (product_id) REFERENCES products(id)
+CREATE TABLE IF NOT EXISTS peminjaman (
+	id_pinjam INTEGER PRIMARY KEY AUTOINCREMENT,
+	id_buku INTEGER,
+	id_user INTEGER,
+	tgl_pinjam TEXT,
+	CONSTRAINT peminjaman_FK FOREIGN KEY (id_user) REFERENCES "user"(id_user) ON DELETE CASCADE,
+	CONSTRAINT peminjaman_FK_1 FOREIGN KEY (id_buku) REFERENCES buku(id_buku) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS sales (
-    id integer not null primary key AUTOINCREMENT,
-    date DATE not null,
-    product_id integer not null,
-    quantity integer not null,
-    FOREIGN KEY (product_id) REFERENCES products(id)
-);
+// CREATE TABLE IF NOT EXISTS sales (
+//     id integer not null primary key AUTOINCREMENT,
+//     date DATE not null,
+//     product_id integer not null,
+//     quantity integer not null,
+//     FOREIGN KEY (product_id) REFERENCES products(id)
+// );
 
-INSERT INTO users(username, password, role, loggedin) VALUES
-    ('aditira', '1234', 'admin', false),
-    ('dina', '4321', 'employee', false),
-    ('dito', '2552', 'employee', false);
+// INSERT INTO users(username, password, role, loggedin) VALUES
+//     ('aditira', '1234', 'admin', false),
+//     ('dina', '4321', 'employee', false),
+//     ('dito', '2552', 'employee', false);
 
 INSERT INTO products(product_name, category, price, quantity) VALUES
     ('Orange', 'Fruits', 5000, 100),
--- backend/repository/entity.go
+++ backend/repository/entity.go
@@ -1,9 +1,7 @@
 package repository
 
-import "time"
-
-type User struct {
-	ID       int64  `db:"id"`
+type user struct {
+	ID       int64  `db:"id_user"`
 	Username string `db:"username"`
 	Password string `db:"password"`
 	Role     string `db:"role"`
@@ -11,40 +9,40 @@ type User struct {
 	Token    string `db:"token"`
 }
 
-type Product struct {
-	ID          int64  `db:"id"`
-	Category    string `db:"category"`
-	ProductName string `db:"product_name"`
+type buku struct {
+	ID          int64  `db:"id_buku"`
+	Category    string `db:"pengarang"`
+	ProductName string `db:"judul_buku"`
+	Price       int    `db:"tahun_terbit"`
+	Quantity    int    `db:"no_buku"`
+}
+
+type peminjaman struct {
+	ID          int64  `db:"id_pinjam"`
+	Category    string `db:"id_buku"`
+	ProductID   int64  `db:"id_user"`
+	ProductName string `db:"tgl_pinjam"`
 	Price       int    `db:"price"`
 	Quantity    int    `db:"quantity"`
 }
 
-type CartItem struct {
-	ID          int64  `db:"id"`
-	Category    string `db:"category"`
-	ProductID   int64  `db:"product_id"`
-	ProductName string `db:"product_name"`
-	Price       int    `db:"price"`
-	Quantity    int    `db:"quantity"`
-}
+// type Sales struct {
+// 	ID          int64     `db:"id"`
+// 	Date        time.Time `db:"date"`
+// 	Category    string    `db:"category"`
+// 	ProductID   int64     `db:"product_id"`
+// 	ProductName string    `db:"product_name"`
+// 	Price       int       `db:"price"`
+// 	Quantity    int       `db:"quantity"`
+// 	Total       int       `db:"total"`
+// }
 
-type Sales struct {
-	ID          int64     `db:"id"`
-	Date        time.Time `db:"date"`
-	Category    string    `db:"category"`
-	ProductID   int64     `db:"product_id"`
-	ProductName string    `db:"product_name"`
-	Price       int       `db:"price"`
-	Quantity    int       `db:"quantity"`
-	Total       int       `db:"total"`
-}
-
-type GetSalesRequest struct {
-	StartPeriod *time.Time `db:"start_period"`
-	EndPeriod   *time.Time `db:"end_period"`
-	ProductID   int64      `db:"product_id"`
-	ProductName string     `db:"product_name"`
-}
+// type GetSalesRequest struct {
+// 	StartPeriod *time.Time `db:"start_period"`
+// 	EndPeriod   *time.Time `db:"end_period"`
+// 	ProductID   int64      `db:"product_id"`
+// 	ProductName string     `db:"product_name"`
+// }
 
 func (r GetSalesRequest) IsEmptyRequest() bool {
 	if r.StartPeriod == nil && r.EndPeriod == nil && r.ProductName == "" {
